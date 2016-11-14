264b3d6ce2f44e214eebb13dbc6738792dadba3c
Ethan Daley <ethandaley@admins-mini-2.dnsdhcp.provo.novell.com>
2016-11-14 16:19:27 -0700

    updates for the creating a list

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+
+// $(document).ready(function() {
+
+// // 	var list = document.getElementById('testing');
+
+// // 	function dropname() {
+// // 		var addname = document.getElementById('addname').value;
+// // 		var entry = document.createElement('li');
+// // 		entry.appendChild(document.createTextNode(addname));
+// // 		list.appendChild(entry);
+// // 	}
+
+		
+// 	var list = document.getElementById('demo');
+
+// 	function addname() {
+// 	    var firstname = document.getElementById('firstname').value;
+// 	    var entry = document.createElement('li');
+// 	    entry.appendChild(document.createTextNode(firstname));
+// 	    list.appendChild(entry);
+// 	}
+// });
